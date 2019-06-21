ac961cf52f90ffa61d00b62395dd3b5cd593a69e
Hugo Rojas <hurojas.rios@gmail.com>
2019-06-20 23:07:54 -0400

    hollli

--- formulario.js
+++ formulario.js
@@ -1,33 +1,31 @@
 
+
+
 var validator = require('validator');
-// && !validator.isIdentityCard(ingresonumerotarjeta)
-document.getElementById("forma").addEventListener('submit', function(e){
-    e.preventDefault();
-    var todoBien = true;
 
-    let ingresocorreo =document.getElementById("datoscorreo").value;
-    let ingresonumerotarjeta=document.getElementById("datostarjeta").value;
+
+
     
-    if(!validator.isEmail(ingresocorreo))
+
+document.getElementById("forma").addEventListener('submit', function(){
+    let formatomail=document.getElementById("datoscorreo").value;
+    let datosnombre=document.getElementById("datoscorreo").value;
+    let datosapellido=document.getElementById("datoscorreo").value;
+    if (validator.isEmail(formatomail)===true &&
+        validator.isEmpty(datosnombre)===false &&
+        validator.isEmpty(datosapellido)===false
+          
+    )
+       
+   
+    
     {
-        document.querySelector("#avisoEmail").innerHTML = "datos incorrectos";
-        todoBien = false;
-    }else{
-        document.querySelector("#avisoEmail").innerHTML = "";
-        todoBien = true;
+        alert ("sending")
+    } else {
+        alert ("verificar ....datos incorrectos");
     }
-
-    if(!validator.isIdentityCard(ingresonumerotarjeta))
-    {
-        document.querySelector("#avisoTarjeta").innerHTML = "datos incorrectos";
-        todoBien = false;
-    }else{
-        document.querySelector("#avisoTarjeta").innerHTML = "";
-        todoBien = true;
-    }
-
-    if(todoBien){
-        e.target.submit();
-    }
-});
-
+    
+   
+    
+}
+);
